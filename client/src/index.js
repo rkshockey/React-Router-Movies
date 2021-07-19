a50c238b4529a17e9d799106b29051fba76353f6
rkshockey <rkshockey@gmail.com>
2021-07-19 14:24:50 -0400

    Wrapped App in BrowserRouter

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter } from 'react-router-dom'
+
 // You'll need to wrap <App /> for routing to work
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+, document.getElementById('root'));
